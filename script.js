5500936d84f55a3a28bbd3a78f274071a945f8d9
Jacob Lee <jacobl11@uci.edu>
2022-11-05 12:55:37 -0700

    Init MongoDB

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 const express = require('express')
+// const { MongoClient } = require("mongodb");
 const app = express()
 const port = 3000
 const cors = require('cors')
 
+// const uri = "mongodb+srv://zotclash:<zothacks2022>@zotclash.jsufize.mongodb.net/?retryWrites=true&w=majority"
+// const client = new MongoClient(uri);
+// async function run() {
+//     try {
+//       // Connect the client to the server (optional starting in v4.7)
+//       await client.connect();
+//       // Establish and verify connection
+//       await client.db("admin").command({ ping: 1 });
+//       console.log("Connected successfully to server");
+//     } finally {
+//       // Ensures that the client will close when you finish/error
+//       await client.close();
+//     }
+//   }
+//   run().catch(console.dir);
 const dining_halls = {
     "dining_halls":[
         {"name": "Brandywine", "image": "https://www.ocregister.com/wp-content/uploads/2019/09/OCR-L-UCIDORMS-0921-12-JAG.jpg?w=620"},
