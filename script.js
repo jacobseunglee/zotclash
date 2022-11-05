27ee386d7cfd3ffcdc75a41c3f1421a2bfae3dcb
Julius Yang <derpyjul45@outlook.com>
2022-11-05 14:04:43 -0700

    Init MongoDB connection

--- script.js
+++ script.js
@@ -1,24 +1,35 @@
 const express = require('express')
-// const { MongoClient } = require("mongodb");
+const { MongoClient } = require("mongodb");
 const app = express()
 const port = 3000
 const cors = require('cors')
 
-// const uri = "mongodb+srv://zotclash:<zothacks2022>@zotclash.jsufize.mongodb.net/?retryWrites=true&w=majority"
-// const client = new MongoClient(uri);
-// async function run() {
-//     try {
-//       // Connect the client to the server (optional starting in v4.7)
-//       await client.connect();
-//       // Establish and verify connection
-//       await client.db("admin").command({ ping: 1 });
-//       console.log("Connected successfully to server");
-//     } finally {
-//       // Ensures that the client will close when you finish/error
-//       await client.close();
-//     }
-//   }
-//   run().catch(console.dir);
+async function main() {
+  const uri = "mongodb+srv://ZotClash:Zothacks2022@zotclash.jegpvvx.mongodb.net/?retryWrites=true&w=majority"
+  const client = new MongoClient(uri);
+  try{
+    await client.connect();
+    await listDatabases(client);
+  }
+  catch (e)
+  {
+    console.error(e);
+  }
+  finally
+  {
+    await client.close();
+  }
+
+}
+
+async function listDatabases(client){
+  databasesList = await client.db().admin().listDatabases();
+
+  console.log("Databases:");
+  databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
+
+main().catch(console.error);
 const dining_halls = {
     "dining_halls":[
         {"name": "Brandywine", "image": "https://www.ocregister.com/wp-content/uploads/2019/09/OCR-L-UCIDORMS-0921-12-JAG.jpg?w=620"},
