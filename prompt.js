de8b780275e38b69745db86b8d917ae163d8d545
Jacob Lee <jacobl11@uci.edu>
2022-11-05 18:09:06 -0700

    Implemented prompt req, returns pid given sid

--- prompt.js
+++ prompt.js
@@ -1,10 +1,17 @@
+const mongo = require('mongodb');
 function handlePrompt(db)
 {
     return function(req, res)
     {
         console.log("from prompt");
-        sid = req.query.sid
-        db.collection("Prompts").findOne({_id: sid}).then(x => console.log(x))
+        var sid = req.query.sid
+        sid = new mongo.ObjectId(sid);
+        console.log(sid)
+        db.collection("Sessions").findOne({_id: sid}).then(session => {
+            console.log(session)
+            const pid = session.prompts[session.index]
+            res.send({is:pid})
+        })
     }
     
 }
