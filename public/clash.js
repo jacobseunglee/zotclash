33a3ac676294da25f304c85b0ae4e98179b7c13c
Caleb Yang <kb@jaymoo.com>
2022-11-05 21:12:15 -0700

    finalizing data fetch, prep for publish

--- public/clash.js
+++ public/clash.js
@@ -1,34 +1,34 @@
 const IMAGES_TO_SHOW = 10
 let numPairsShown = 0
 
-
-
 myimg1 = document.getElementById("image1")
 myimg2 = document.getElementById("image2")
 text1 = document.getElementById("text1")
 text2 = document.getElementById("text2")
 
+function getStats() {
+    fetch('../stats')
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data);
+    })
+}
+
 function grabImgData(chosen) {
 
     if (chosen == 1) {
-        fetch(`http://localhost:3000/inc?name=${text1.textContent}`)
+        fetch(`../inc?name=${text1.textContent}`)
     }
     else if (chosen == 2) {
-        fetch(`http://localhost:3000/inc?name=${text2.textContent}`)
+        fetch(`../inc?name=${text2.textContent}`)
     }
 
     if (numPairsShown == IMAGES_TO_SHOW) { // Last choice selected!
-        fetch('http://localhost:3000/stats')
-        .then((response) => response.json())
-        .then((data) => {
-            console.log(data)
-            stats = data
-        })
-        
-        window.location.href = "http://localhost:3000/stats.html";
+        getStats();
+        window.location.href = "../public/stats.html";
     }
-
-    fetch(`http://localhost:3000/prompt?sid=${SESSION_ID.id}`)
+    
+    fetch(`../prompt?sid=${SESSION_ID.id}`)
     .then((response) => response.json())
     .then((data) => {
 
@@ -36,19 +36,23 @@ function grabImgData(chosen) {
         choice1 = data.choices[0];
         choice2 = data.choices[1];
 
-        text1.textContent = choice1.name;
-        text2.textContent = choice2.name;
+        if (text1 && text2 && myimg1 && myimg2) {
+            text1.textContent = choice1.name;
+            text2.textContent = choice2.name;
+            myimg1.src = choice1.image;
+            myimg2.src = choice2.image;
 
-        myimg1.src = choice1.image;
-        myimg2.src = choice2.image;
+            numPairsShown++;
+        }
+        
 
-        numPairsShown++;
+        
         
     });
 
 }
 
-fetch('http://localhost:3000/session')
+fetch('../session')
 .then((response) => response.json())
 .then((data) => {
     SESSION_ID = data;
