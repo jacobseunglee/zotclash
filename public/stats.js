515e893cc0b58f5b85b202f0c3e6e8092307a962
Julius Yang <derpyjul45@outlook.com>
2022-11-05 21:17:49 -0700

    fixed stats.js

--- public/stats.js
+++ public/stats.js
@@ -1,11 +1,25 @@
-import {PieChart} from "@d3/pie-chart"
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
 
+function drawChart() {
 
-charts = []
+  var data = google.visualization.arrayToDataTable([
+    ['Task', 'Hours per Day'],
+    ['Work',     100],
+    ['Eat',      2],
+    ['Commute',  2],
+    ['Watch TV', 2],
+    ['Sleep',    7]
+  ]);
 
-dining_chart = PieChart(selections, {
-    name: d => d.name,
-    value: d => d.value,
-    width,
-    height: 500
-  })
+  var options = {
+    title: 'My Daily Activities',
+    legend: {position: 'none'},
+    backgroundColor: 'transparent',
+    chartArea: {left:0,top:0,width:'100%',height:'100%'}
+  };
+
+  var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+  chart.draw(data, options);
+}
