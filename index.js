dc5d44883454542f4edba664deec3f14aac8982f
Jacob Lee <jacobl11@uci.edu>
2022-11-05 17:27:28 -0700

    Changed connection behavior with MongoDB client, init prompt.js

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ var url = process.env.MONGOLAB_URI;
 
 async function main() {
   const uri = url;
-  const client = new MongoClient(uri);
+  client = new MongoClient(uri);
   const database = client.db("ClashData");
-  const prompt = require("./prompt");
+  const prompt = require("./prompt")(database);
   const session = require("./session")(database);
   try{  
     await client.connect();
@@ -21,28 +21,22 @@ async function main() {
   catch (e)
   {
     console.error(e);
-  }
-  finally
-  {
     await client.close();
   }
 
-  app.use(cors())
 
+app.use(cors())
+app.get('/prompt', prompt.handlePrompt)
+app.get('/session', session.handleSession)
 app.get('/', (req, res) => {
   randInt = randomIntFromInterval(0, pairs.length-1);
   res.send(pairs[randInt])
   pairs.splice(randInt, 1); // Remove that element from the list so it's not repeated
 })
 
-app.get('/prompt', prompt.handlePrompt)
-
-app.get('/session', session.handleSession)
-
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
-
 }
 
 async function listDatabases(client){
@@ -53,80 +47,8 @@ async function listDatabases(client){
 };
 
 main().catch(console.error);
-
-const dining_halls = {
-    "choices":[
-        {"name": "Brandywine", "image": "https://www.ocregister.com/wp-content/uploads/2019/09/OCR-L-UCIDORMS-0921-12-JAG.jpg?w=620"},
-        {"name": "Anteatery", "image": "https://global-uploads.webflow.com/5eec789d24d891b6d1d15438/5f20833538e3463a336d44ff_b02_RSM-Design_Anteatery-Food-Hall_College-Food-Hall-Design.jpg"}
-    ]
-}
-
-const housing = {
-  "choices":[
-      {"name": "Middle Earth", "image": ""},
-      {"name": "Mesa Court", "image": ""}
-  ]
-}
-
-const libraries = {
-  "choices":[
-      {"name": "Langson Library", "image": ""},
-      {"name": "Science Library", "image": ""}
-  ]
-}
-
-const recreation = {
-"choices":[
-      {"name": "eSports Arena", "image": ""},
-      {"name": "Anteater Recreation Center", "image": ""}
-  ]
-}
-
-const convenience = {
-  "choices":[
-      {"name": "Zot N Go", "image": ""},
-      {"name": "Sidedoor", "image": ""}
-  ]
-}
-
-const OS = {
-  "choices":[
-      {"name": "Mac", "image": ""},
-      {"name": "Windows", "image": ""}
-  ]
-}
-
-const boba = {
-  "choices":[
-      {"name": "Cha", "image": ""},
-      {"name": "Sharetea", "image": ""}
-  ]
-}
-
-const sports = {
-  "choices":[
-      {"name": "Basketball", "image": ""},
-      {"name": "Volleyball", "image": ""}
-  ]
-}
-
-const flasks = {
-  "choices":[
-      {"name": "Hydro Flask", "image": ""},
-      {"name": "Thermoflask", "image": ""}
-  ]
-}
-
-const studyspots = {
-  "choices":[
-      {"name": "Study in Aldrich Park", "image": ""},
-      {"name": "Study in a library", "image": ""}
-  ]
-}
-
-pairs = [dining_halls, housing, libraries, recreation, convenience, OS, boba, sports, flasks, studyspots]
+client.close();
 
 function randomIntFromInterval(min, max) { // min and max included 
   return Math.floor(Math.random() * (max - min + 1) + min)
 }
-
