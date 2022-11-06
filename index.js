cf2b3f871e4f5500015ae3c7b1601cd1df18261b
Caleb Yang <kb@jaymoo.com>
2022-11-05 18:05:11 -0700

    Added all images and updated game UI

--- index.js
+++ index.js
@@ -39,6 +39,8 @@ app.get('/prompt', prompt.handlePrompt)
 
 app.get('/session', session.handleSession)
 
+app.use('/public', express.static('public'))
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
@@ -63,64 +65,64 @@ const dining_halls = {
 
 const housing = {
   "choices":[
-      {"name": "Middle Earth", "image": ""},
-      {"name": "Mesa Court", "image": ""}
+      {"name": "Middle Earth", "image": "https://newuniversity.org/wp-content/uploads/2019/10/DSC_0004.jpg"},
+      {"name": "Mesa Court", "image": "https://images.squarespace-cdn.com/content/v1/5b60d4fa70e802968763e7f5/1541525539929-5SEX9R8F856SGNI6TE6W/Housing1.jpg?format=1000w"}
   ]
 }
 
 const libraries = {
   "choices":[
-      {"name": "Langson Library", "image": ""},
-      {"name": "Science Library", "image": ""}
+      {"name": "Langson Library", "image": "https://i0.wp.com/interns.society19.com/wp-content/uploads/2018/07/LLIB-2.jpg?ssl=1"},
+      {"name": "Science Library", "image": "https://classrooms.uci.edu/files/2022/02/Science-Library-1-1024x768.png"}
   ]
 }
 
 const recreation = {
 "choices":[
-      {"name": "eSports Arena", "image": ""},
-      {"name": "Anteater Recreation Center", "image": ""}
+      {"name": "eSports Arena", "image": "https://esports.uci.edu/wp-content/uploads/sites/3/2020/10/Arena-1500x500.jpg"},
+      {"name": "Anteater Recreation Center", "image": "https://www.campusrec.uci.edu/arc/images/arc-insideP.jpg"}
   ]
 }
 
 const convenience = {
   "choices":[
-      {"name": "Zot N Go", "image": ""},
-      {"name": "Sidedoor", "image": ""}
+      {"name": "Zot N Go", "image": "https://pr0.nicelocal.com/arcQQS2UvbOCnlgRrpFSlQ/1500x1125,q75/4px-BW84_n0QJGVPszge3NRBsKw-2VcOifrJIjPYFYkOtaCZxxXQ2TEuHbn5_ylLLHUZzR_RLtfU_zmfS8V2y_kV6z5MajtJgQbnGDQHCmw5fFEg85UQKw"},
+      {"name": "Sidedoor", "image": "https://www.food-management.com/sites/food-management.com/files/uploads/2014/09/unnamed_14.jpg"}
   ]
 }
 
 const OS = {
   "choices":[
-      {"name": "Mac", "image": ""},
-      {"name": "Windows", "image": ""}
+      {"name": "Mac", "image": "https://149367133.v2.pressablecdn.com/wp-content/uploads/2021/05/gadgetmatch-apple-2021-mac-red-pink-05.jpg"},
+      {"name": "Windows", "image": "https://cdn.mos.cms.futurecdn.net/o8g2bEtp4dfmDPTdjavRGf-1920-80.jpg.webp"}
   ]
 }
 
 const boba = {
   "choices":[
-      {"name": "Cha", "image": ""},
-      {"name": "Sharetea", "image": ""}
+      {"name": "Cha", "image": "https://www.ucieas.com/uploads/3/0/0/9/30093831/cha-for-tea_1_orig.jpg"},
+      {"name": "Sharetea", "image": "https://s3-media0.fl.yelpcdn.com/bphoto/duGbTGRunN4gNhoMlzwxWw/o.jpg"}
   ]
 }
 
 const sports = {
   "choices":[
-      {"name": "Basketball", "image": ""},
-      {"name": "Volleyball", "image": ""}
+      {"name": "Basketball", "image": "https://upload.wikimedia.org/wikipedia/commons/d/db/UCI_Bren_Events_Center_Basketball_Court_2008.jpg"},
+      {"name": "Volleyball", "image": "https://gomatadors.com/images/2022/9/23/Orshoff_4168_SLg6J.jpg?width=1920&quality=80&format=jpg"}
   ]
 }
 
 const flasks = {
   "choices":[
-      {"name": "Hydro Flask", "image": ""},
-      {"name": "Thermoflask", "image": ""}
+      {"name": "Hydro Flask", "image": "https://hips.hearstapps.com/hmg-prod/images/screen-shot-2018-12-21-at-4-12-41-pm-1545427220.png?crop=1.00xw:0.898xh;0,0.0679xh&resize=1200:*"},
+      {"name": "Thermoflask", "image": "https://prod-cdn-thekrazycouponlady.imgix.net/wp-content/uploads/2019/10/thermoflask-2-10-8-costco-1570538447.jpg"}
   ]
 }
 
 const studyspots = {
   "choices":[
-      {"name": "Study in Aldrich Park", "image": ""},
-      {"name": "Study in a library", "image": ""}
+      {"name": "Study in Aldrich Park", "image": "https://miro.medium.com/max/1400/0*2-oC29n_3m84Pkub"},
+      {"name": "Study in a library", "image": "https://www.lib.uci.edu/sites/default/files/img/library-floor/asl45-study-v3.jpg"}
   ]
 }
 
--- public/clash.js
+++ public/clash.js
@@ -1,21 +1,21 @@
 const IMAGES_TO_SHOW = 10
 let numPairsShown = 0
 
+
+myimg1 = document.getElementById("image1")
+myimg2 = document.getElementById("image2")
+text1 = document.getElementById("text1")
+text2 = document.getElementById("text2")
+
 function grabImgData() {
 
     if (numPairsShown == IMAGES_TO_SHOW) { // Last choice selected!
         window.location.href = "http://localhost:3000/stats.html";
     }
 
-    myimg1 = document.getElementById("image1")
-    myimg2 = document.getElementById("image2")
-    text1 = document.getElementById("text1")
-    text2 = document.getElementById("text2")
     fetch('http://localhost:3000')
     .then((response) => response.json())
     .then((data) => {
-
-        
 
         choice1 = data.choices[0];
         choice2 = data.choices[1];
@@ -24,7 +24,7 @@ function grabImgData() {
         text2.textContent = choice2.name;
 
         myimg1.src = choice1.image;
-        myimg2.src = choice1.image;
+        myimg2.src = choice2.image;
 
         numPairsShown++;
         
