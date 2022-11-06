c6af8a18d9f5e2ea59e6a00944546af2a633cfa4
Jacob Lee <jacobl11@uci.edu>
2022-11-05 20:51:15 -0700

    allow env PORT

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { MongoClient } = require("mongodb");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const cors = require('cors');
 
 var dotenv = require('dotenv');
